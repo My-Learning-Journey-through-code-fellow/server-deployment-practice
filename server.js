cf8221b7136a5f2480cd3eb6871c5494caba6b62
Trace Dugar <tracedugar89@gmail.com>
2022-11-29 07:56:52 -0800

    Began exercise will finish soon

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 3002;
+
+
+//This is a singleton (which is a single instance)
+const app = express();
+
+
+// middleware - funtions that interact with REQ / RES obj
+app.use(cors());
+
+// app.use();
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('Hi World');
+});
+
+
+function start(){
+  app.listen(PORT, () => console.log('listening on port: ', PORT));
+}
+
+module.exports =  { start, app };
