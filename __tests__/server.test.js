966c6fcf26b738a2170b8cecc75489230a1d8a16
Trace Dugar <tracedugar89@gmail.com>
2022-11-30 18:01:31 -0800

    add helloPath , helloQuery, routes, and associated tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -21,4 +21,17 @@ describe('APIServer', () => {
     expect(response.status).toEqual(500);
     expect(response.body.route).toEqual('/bad');
   });
+
+  it('works with query params and the "/helloQuery" route', async () => {
+    const response = await request.get('/helloQuery?name=Trace');
+
+    expect(response.text).toEqual('Hello Trace');
+  });
+
+  it('works with path params and the "/helloPath" route', async () => {
+    const response = await request.get('/helloPath/Abigail');
+
+    expect(response.text).toEqual('Hello Abigail');
+  });
+
 });
